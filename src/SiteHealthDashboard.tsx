90d7c8e5ada92c041335977762a805c927b3624e
Barbara Taskinen <barbara.taskinen@gmail.com>
2025-12-10 10:15:51 +0200

    Create SiteHealthDashboard

---
+++ src/SiteHealthDashboard.tsx
@@ -0,0 +1,13 @@
+import './SiteHealthDashboard.css'
+import { Header } from './components/Header'
+
+export const SiteHealthDashboard = () => {
+  return (
+    <div className='SiteHealthDashboard'>
+      <Header />
+      <div className='SiteHealthDashboard__mainContent'>
+        Content
+      </div>
+    </div>
+  )
+}
